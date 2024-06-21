1edc1bf95ecd6e3b63c65b855e67ba7b6218faa0
KPJ1315 <karthikpj1315@gmail.com>
2024-06-22 00:41:29 +0530

    Resolved Cucumber report issue and Generated one cucumber report for first test case

--- src/test/java/baseclass/AppBase.java
+++ src/test/java/baseclass/AppBase.java
@@ -9,6 +9,7 @@ import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
+import io.cucumber.java.After;
 
 public class AppBase {
 
@@ -62,6 +63,7 @@ public class AppBase {
         }
     }
 
+    @After
     public void endSession() {
         if (driver != null) {
             driver.quit();
