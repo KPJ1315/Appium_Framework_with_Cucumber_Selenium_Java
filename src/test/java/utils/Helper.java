27f88616d28f0f0325264071bf4c6c89ca59146e
KPJ1315 <karthikpj1315@gmail.com>
2024-07-24 23:29:21 +0530

    Added couple of test cases

--- src/test/java/utils/Helper.java
+++ src/test/java/utils/Helper.java
@@ -1,7 +1,58 @@
 package utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class Helper {
 	
+	static XSSFWorkbook workbook;
+	static XSSFSheet sheet;
+	
+	public static String[] readExcelData(String filePath, String sheetName) {
+
+		List<String> listStr = new ArrayList<String>(); 
+
+		try {
+			File excelFile = new File(filePath);
+
+			if(!excelFile.exists()) {
+				System.out.println("File doesn't exists!!!");
+				//				return null;
+				return null;
+			}
+			FileInputStream fis = new FileInputStream(excelFile);
+			workbook = new XSSFWorkbook(excelFile);
+
+			sheet = workbook.getSheet(sheetName); 
+			int rows = sheet.getPhysicalNumberOfRows();
+			for(int i = 1;i<rows;i++) {
+				Row row = sheet.getRow(i);				
+				String s = row.getCell(0).toString();
+				listStr.add(s);				
+			}
+			fis.close();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String arrStr[] = new String[listStr.size()];
+		for(int i=0;i<arrStr.length;i++) {
+			arrStr[i]=listStr.get(i);
+		}
+
+		Arrays.sort(arrStr);
+		return arrStr;		
+
+	}
+
 	
 
 }
